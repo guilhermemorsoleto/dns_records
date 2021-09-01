3e4fdd7e40edc8ae4c927d332e52869bc9864f7f
Guilherme Morsoleto <g.morsoleto@gmail.com>
2021-09-01 00:20:37 -0300

    Added get dns_records

--- app/controllers/api/v1/dns_records_controller.rb
+++ app/controllers/api/v1/dns_records_controller.rb
@@ -3,7 +3,27 @@ module Api
     class DnsRecordsController < ApplicationController
       # GET /dns_records
       def index
-        # TODO: Implement this action
+        # Where should I use page?
+        page = params[:page]
+        return render json: { status: 422, error: "page is required" } unless page.present?
+        hostnames = []
+        total_records = 0
+        included = params[:included].try(:split, ',')
+        excluded = params[:excluded].try(:split, ',')
+
+        # Needs refactory
+        @dns_records = DnsRecord.all
+        @dns_records.each do |dns_record|
+          records = dns_record.hostnames.included(included).excluded(excluded)
+          if records.present?
+            # Needs to add count number of hostnames
+            hostnames.push(records)
+            total_records = total_records + 1
+          end
+        end
+
+        # Without each loop with the proper query we can change this return
+        render json: { records: @dns_records, related_hostnames: hostnames, total_records: total_records }
       end
 
       # POST /dns_records
--- app/models/hostname.rb
+++ app/models/hostname.rb
@@ -1,2 +1,5 @@
 class Hostname < ApplicationRecord
+
+  scope :included, -> (hostname) { where(hostname: hostname) if hostname.present? }
+  scope :excluded, -> (hostname) { where.not(hostname: hostname)  if hostname.present? }
 end
